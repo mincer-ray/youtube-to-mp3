eea1fc6fea0c9ef6785e91d08535701bf64d7721
Peter De Genaro <pdegenaro@gmail.com>
2018-12-25 21:33:04 -0500

    refactor and update html

--- scripts/yt2mp3.js
+++ scripts/yt2mp3.js
@@ -5,26 +5,43 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-const toMp3 = (url, name) => {
+const toMp3 = (url) => {
   let start = Date.now();
 
   let stream = ytdl(url, {
-    quality: 'highestaudio',
+    // quality: 'highestaudio',
+    format: 'best',
   });
   
-  return new Promise((resolve, reject) => {
-    ffmpeg(stream)
-      .audioBitrate(128)
-      .save(`${__dirname}/../output/${name}.mp3`)
-      .on('progress', (p) => {
-        readline.cursorTo(process.stdout, 0);
-        process.stdout.write(`${p.targetSize}kb downloaded`);
-      })
-      .on('end', () => {
-        fs.rename(`${__dirname}/../output/${name}.mp3`, `${__dirname}/../output/${name}_finished.mp3`)
-        console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`)
-        resolve();
-      });
+  return ytdl.getInfo(url, {
+    // quality: 'highestaudio',
+    format: 'best',
+  }).then((data) => {
+    const totalTime = parseInt(data.length_seconds, 10);
+    const name = data.title;
+
+    return new Promise((resolve, reject) => {
+      ffmpeg(stream)
+        .audioBitrate(128)
+        .save(`${__dirname}/../output/${name}.mp3`)
+        .on('progress', (p) => {
+          const timeData = p.timemark.split('.')[0].split(':');
+          let convertedTime = 0;
+          convertedTime = convertedTime + parseInt(timeData[2]);
+          convertedTime = convertedTime + parseInt(timeData[1]) * 60;
+          convertedTime = convertedTime + parseInt(timeData[0]) * 60 * 60;
+          const timeMark = parseInt(p.timemark, 10);
+
+          readline.cursorTo(process.stdout, 0);
+          console.log(`${Math.floor((convertedTime / totalTime) * 100)}%`)
+          // process.stdout.write(`${p.targetSize}kb downloaded`);
+        })
+        .on('end', () => {
+          fs.rename(`${__dirname}/../output/${name}.mp3`, `${__dirname}/../output/${name}_finished.mp3`)
+          console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`)
+          resolve();
+        });
+    });
   });
 }
 
